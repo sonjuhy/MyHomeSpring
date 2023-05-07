0191e50a8bc6ab03943708c78b2cb95d6ed68ce5
Red Hat <sonjuhy@gmail.com>
2023-05-07 21:21:53 +0900

    🚨 Allow CROS on Chrome
    - Allow CROS on Chrome

--- src/main/java/com/myhome/server/config/WebSecurityConfig.java
+++ src/main/java/com/myhome/server/config/WebSecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -97,9 +100,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
