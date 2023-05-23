ca95167e3f3a30ef12ab099bec82289870d79da1
Red Hat <sonjuhy@gmail.com>
2023-05-23 15:29:21 +0900

    ✨ Remove MQTT
    - Remove MQTT lib
    - Remove MQTT code

--- src/main/java/com/myhome/server/ServerApplication.java
+++ src/main/java/com/myhome/server/ServerApplication.java
@@ -2,10 +2,8 @@ package com.myhome.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.integration.annotation.IntegrationComponentScan;
 
 @SpringBootApplication
-@IntegrationComponentScan
 public class ServerApplication {
 
 	public static void main(String[] args) {
