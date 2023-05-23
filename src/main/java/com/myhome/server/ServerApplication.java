74acbfaa3a95fd3f7b437854310586280ecde506
Red Hat <sonjuhy@gmail.com>
2023-05-23 15:36:16 +0900

    📝 Remove about db info
    - 내용1
    - 내용2

--- src/main/java/com/myhome/server/ServerApplication.java
+++ src/main/java/com/myhome/server/ServerApplication.java
@@ -8,19 +8,5 @@ public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-//		StandardPBEStringEncryptor pbe = new StandardPBEStringEncryptor();
-//		pbe.setAlgorithm("PBEWithMD5AndDES");
-//		pbe.setPassword("myhomeProjectKey");
-//
-//		String enc = pbe.encrypt("sonjuhy_home");
-//		System.out.println("enc : " + enc);
-//
-//		enc = pbe.encrypt("son278298@");
-//		System.out.println("enc : " + enc);
-//
-//		enc = pbe.encrypt("jdbc:mysql://sonjuhy.iptime.org:3306/HomeUpdate?serverTimezone=Asia/Seoul");
-//		System.out.println("enc : " + enc);
-//		String des = pbe.decrypt(enc);
-//		System.out.println("des : " + des);
 	}
 }
